141b9ded2fc74c60fb2af5a223c7d46000212ac5
krishna <krishnaprasad284@gmail.com>
2024-09-07 00:38:59 +0530

    add dark mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { HStack } from "@chakra-ui/react";
-import { Image, Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
+import ColorModeSwitch from "./ColorModeWitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
 };
