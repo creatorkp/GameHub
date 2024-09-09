69111cdb82b612e8a939d6cc964edddfeb98e9b7
krishna <krishnaprasad284@gmail.com>
2024-09-09 16:35:21 +0530

    GenreImage + optimize side and main grid width+ loading for Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,30 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import CropImage from "./CropImage";
+import { error } from "console";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, errors, isloading } = useGenres();
+
+  if (errors) return null;
+  if (isloading) return <Spinner />;
   return (
-    <ul>
-      {data.map((catog) => (
-        <li key={catog.id}>{catog.name}</li>
-      ))}
-    </ul>
+    <>
+      <List>
+        {data.map((catog) => (
+          <ListItem key={catog.id} padding="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={CropImage(catog.image_background)}
+              ></Image>
+              <Text fontSize="lg">{catog.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
